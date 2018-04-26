ccedea0ec79aeb92be222d221c9d781cb5765652
Jijil <jijil.kakkadathu@cognizant.com>
2018-04-26 21:33:27 +0530

    Deleted old project

--- src/main/java/com/wt/model/Category.java
+++
@@ -1,8 +0,0 @@
-package com.wt.model;
-
-public class Category {
-
-	private String _catid;
-	private String categoryName;
-	
-}
--- src/main/java/com/wt/services/intf/WorkoutServiceInterface.java
+++
@@ -1,9 +0,0 @@
-package com.wt.services.intf;
-
-import com.wt.model.Category;
-
-public interface WorkoutServiceInterface {
-
-	String addCategory(Category category);
-
-}
