b29be2bd7f6d11d845b09c5bd558a781183bbf35
Jijil <jijil.kakkadathu@cognizant.com>
2018-05-29 16:24:40 +0530

    War changes

--- src/main/java/com/workoutTracker/wt/WtApplication.java
+++ src/main/java/com/workoutTracker/wt/WtApplication.java
@@ -2,12 +2,20 @@ package com.workoutTracker.wt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-public class WtApplication {
+public class WtApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WtApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(
+			SpringApplicationBuilder application) {
+		return application.sources(WtApplication.class);
 	}
 }
